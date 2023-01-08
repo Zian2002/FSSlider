e452bf94b5c26676d7ae7b8a9a5ab4f96c3971bb
zian2002 <justinduy81@gmail.com>
2023-01-08 18:30:44 +0700

    done

---
+++ app.js
@@ -0,0 +1,139 @@
+data.sort((a, b) => {
+    if(a.id%2 == 0 && b.id%2 != 0){
+        return 1;
+     };
+     if(a.id%2 != 0 && b.id%2 == 0){
+        return -1;
+     };
+     return a.id - b.id;
+})
+
+const slider = document.querySelector(".slider");
+const item = document.querySelector(".slider-item");
+let itemWidth = item ? item.scrollWidth : 400; //css
+let gap = 20; //css
+const progress = document.querySelector(".progress");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+
+displaySlider(data)
+
+slider.addEventListener("click", function(e){
+    console.log(e.target);
+})
+
+prevButton.addEventListener("click", function(e) {
+    slider.scrollLeft -= itemWidth + gap;
+    displayProgress();
+    displayControlButton();
+    clearInterval(interval);
+})
+
+nextButton.addEventListener("click", function(e) {
+    slider.scrollLeft += itemWidth + gap;
+    displayProgress();
+    displayControlButton();
+    clearInterval(interval);
+})
+
+slider.addEventListener("wheel", function(e){
+    if(getDeviceType() === "desktop" && window.innerWidth > 1023.98) 
+        e.preventDefault();
+    this.scrollLeft += e.deltaY;
+    clearInterval(interval);
+})
+
+slider.addEventListener("scroll", function() {
+    displayProgress();
+    displayControlButton();
+})
+
+window.addEventListener("resize", debounceFn(function(){
+    slider.style = "";
+    let item = document.querySelector(".slider-item");
+    itemWidth = item ? item.scrollWidth : 400; //css
+    slider.style = `grid-template-columns: repeat(${Math.round(data.length / 2)}, ${itemWidth}px);`;
+}, 200))
+
+function createSliderItem({id, src, title, desc}){
+    const template = 
+    `<div class="slider-item" data-id="${id}">
+        <img src="${src}" alt="">
+        <div class="content">
+            <h2 class="title">${title}</h2>
+            <p class="desc">${desc}</p>
+        </div>
+    </div>`;
+    slider.insertAdjacentHTML("beforeend", template);
+}
+
+function displayControlButton(){
+    if (progress.style.width === "100%"){
+        nextButton.classList.add("disabled")
+        prevButton.classList.remove("disabled")
+    }else if (progress.style.width === `${Math.round(slider.clientWidth / slider.scrollWidth * 100)}%`) {
+        nextButton.classList.remove("disabled")
+        prevButton.classList.add("disabled")
+    }else {
+        nextButton.classList.remove("disabled")
+        prevButton.classList.remove("disabled")
+    }
+}
+
+function displayProgress(){
+    pos = slider.scrollLeft + slider.clientWidth;
+    let width = slider.scrollWidth;
+    let length = Math.round((pos / width) * 100);
+    progress.style.width = `${length}%`;
+}
+
+
+
+let interval = setInterval(function(){
+    slider.scrollLeft += itemWidth + 20;
+    displayProgress()
+    if (slider.scrollLeft >= slider.scrollWidth - slider.clientWidth){
+        slider.scrollLeft = 0;
+    }
+}, 3000);
+
+function displaySlider(data){
+    slider.style = `grid-template-columns: repeat(${Math.round(data.length / 2)}, ${itemWidth}px);`;
+    slider.innerHTML = "";
+    data.forEach(item => {
+        createSliderItem(item);
+    })
+    displayProgress()
+    prevButton.classList.add("disabled")
+}
+
+function getDeviceType() {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+      return "tablet";
+    }
+    if (
+      /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+        ua
+      )
+    ) {
+      return "mobile";
+    }
+    return "desktop";
+};
+
+function debounceFn(func, wait, immediate) {
+    let timeout;
+    return function () {
+      let context = this,
+        args = arguments;
+      let later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+}
---
+++ data.js
@@ -0,0 +1,68 @@
+let data = [
+    {
+        "id" : 1,
+        "src" : "https://images.unsplash.com/photo-1667731111091-8c7570bfdefb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "title" : "Title 1",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 2,
+        "src" : "https://images.unsplash.com/photo-1667610342985-f7aad2435341?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "title" : "Title 2",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 3,
+        "src" : "https://images.unsplash.com/photo-1667053508464-eb11b394df83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+        "title" : "Title 3",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 4,
+        "src" : "https://images.unsplash.com/photo-1666819691666-4be36926335e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "title" : "Title 4",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 5,
+        "src" : "https://images.unsplash.com/photo-1667338336416-2fbc1c4424ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "title" : "Title 5",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 6,
+        "src" : "https://images.unsplash.com/photo-1667672118877-f03ed6afb0b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "title" : "Title 6",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 7,
+        "src" : "https://images.unsplash.com/photo-1667793082881-fe34835b2bda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MzN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        "title" : "Title 7",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 8,
+        "src" : "https://images.unsplash.com/photo-1667610342417-2ec8081ac21d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MzB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        "title" : "Title 8",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 9,
+        "src" : "https://images.unsplash.com/photo-1667778770081-02c4afe79472?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        "title" : "Title 6",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 9,
+        "src" : "https://images.unsplash.com/photo-1667114790658-0afc31212d8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=600",
+        "title" : "Title 6",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        "id" : 10,
+        "src" : "https://images.unsplash.com/photo-1667506057340-6f9f6140884f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        "title" : "Title 10",
+        "desc" : "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },    
+]
